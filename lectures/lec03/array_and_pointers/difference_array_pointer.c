b6342ab9522231be0573fe9e22c6720af78f7457
vpbmac <vitorpbarbosa7@gmail.com>
2025-02-05 06:45:53 -0300

    diff array and pointer

---
+++ lectures/lec03/array_and_pointers/difference_array_pointer.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int vector[5] = {1,2,3,4,5};
+    int *pv = vector;
+
+    printf("%zu\n", sizeof(vector));
+    printf("%zu\n", sizeof(pv));
+
+    return 0;
+}
