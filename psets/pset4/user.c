1f7f5e2108bc0f048ab67d7c3f43d16558e0bf03
vpbmac <vitorpbarbosa7@gmail.com>
2025-02-11 09:55:09 -0300

    subtree after and before

--- psets/pset4/user.c
+++ psets/pset4/user.c
@@ -35,19 +35,26 @@ void initializeTree(Node* tree){
 	tree->right = NULL;
 }
 
-void subtree_insert(Node* subtree, Node* newnode){
+void subtree_insert_before(Node* subtree, Node* newnode, COMPARE compare) {
 	//TODO
+
+	// if it has a left, we must be carefull because must after the last one in the left
+	if ()
+
+
+	else:
+		// if it really has no left, so we just put it here in the left part
+		subtree->left = newnode;
+		newnode->parent =subtree; 
+
 }
-void subtree_insert_before(Node* subtree, Node* newnode) {
-	//TODO
-}
-void subtree_insert_after(Node* subtree, Node* newnode) {
+void subtree_insert_after(Node* subtree, Node* newnode, COMPARE compare) {
 	//TODO
 }
 
 
 
-void addNode(Node* subtree, void* element, COMPARE compare){
+void subtree_insert(Node* subtree, void* element, COMPARE compare){
 	// we create the node itself using the element data passed
 	Node* newnode = (Node*) malloc(sizeof(Node));
 	newnode->item = element;
@@ -56,21 +63,21 @@ void addNode(Node* subtree, void* element, COMPARE compare){
 		// if less than we must go left, to insert left
 		// if the left node exists, we must traverse there recursively
 		if (subtree->left != NULL) {
-			subtree_insert(subtree, newnode);
+			subtree_insert(subtree, newnode, compare);
 		}
 		// if the left not exists
 		else {
 			// the predecessor might be going up and after down in a right subtree
-			subtree_insert_before(subtree, newnode);
+			subtree_insert_before(subtree, newnode, compare);
 		}
 	} else {
 		// now we must go right and find where to put it 
 		if (compare(newnode->item, subtree->item)) {
 			if (subtree->right != NULL){
-				subtree_insert(subtree, newnode);
+				subtree_insert(subtree, newnode, compare);
 			} else {
 				// the successor might be going down and left
-				subtree_insert_after(subtree, newnode);
+				subtree_insert_after(subtree, newnode, compare);
 			}
 		}
 		else {
