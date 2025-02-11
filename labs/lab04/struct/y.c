b9e3d1eca1adce11eec3569bea8d24bfbdcf11e1
vpbmac <vitorpbarbosa7@gmail.com>
2025-02-11 08:04:26 -0300

    labs 04

--- labs/lab04/struct/y.c
+++ labs/lab04/struct/y.c
@@ -2,19 +2,20 @@
 #include <stddef.h>
 
 struct x {
-	short s;
     int i;
     char c;
+	short s;
 };
 
 int main() {
     struct x example;
 
     printf("Size of struct x: %zu bytes\n", sizeof(struct x));
-    printf("Offset of 's' in struct x: %zu bytes\n", offsetof(struct x, s));
     printf("Offset of 'i' in struct x: %zu bytes\n", offsetof(struct x, i));
     printf("Offset of 'c' in struct x: %zu bytes\n", offsetof(struct x, c));
+    printf("Offset of 's' in struct x: %zu bytes\n", offsetof(struct x, s));
 
     return 0;
 }
+
 
--- labs/lab04/struct/z.c
+++ labs/lab04/struct/z.c
@@ -2,8 +2,8 @@
 #include <stddef.h>
 
 struct x {
-	short s;
     int i;
+	short s;
     char c;
 };
 
@@ -11,10 +11,12 @@ int main() {
     struct x example;
 
     printf("Size of struct x: %zu bytes\n", sizeof(struct x));
-    printf("Offset of 's' in struct x: %zu bytes\n", offsetof(struct x, s));
     printf("Offset of 'i' in struct x: %zu bytes\n", offsetof(struct x, i));
+    printf("Offset of 's' in struct x: %zu bytes\n", offsetof(struct x, s));
     printf("Offset of 'c' in struct x: %zu bytes\n", offsetof(struct x, c));
 
     return 0;
 }
+
+
 
