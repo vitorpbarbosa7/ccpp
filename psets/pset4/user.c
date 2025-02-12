0b8b9622423a4ec07cd4cbe661c4d97407705f8c
vpbmac <vitorpbarbosa7@gmail.com>
2025-02-12 08:02:59 -0300

    jamais

--- psets/pset4/user.c
+++ psets/pset4/user.c
@@ -2,19 +2,22 @@
 #include "bintree.h"
 
 int main() {
-	Node* tree;
+	Node tree;
 
 	Element* A = createElement('A');
 	Element* B = createElement('B');
 	Element* C = createElement('C');
 
-	// initializeTree(&tree);
+	initializeTree(&tree);
+	
+	// pointer to this kind of function, that is why cast is necessary
+	displayTree(&tree, (DISPLAY)displayElement);
 
-	// subtree_insert(&tree, A, (COMPARE)compareElement);
+	subtree_insert(&tree, A, (COMPARE)compareElement);
 	// subtree_insert(&tree, C, (COMPARE)compareElement);
 	// subtree_insert(&tree, B, (COMPARE)compareElement);
 
-	// displayTree(&tree, (DISPLAY)displayTree);
+	displayTree(&tree, (DISPLAY)displayElement);
 
 
 	free(A);
