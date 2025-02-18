ccb2339f63b75b7c6391115f51b0a69d093b7a49
vpbmac <vitorpbarbosa7@gmail.com>
2025-02-18 08:45:46 -0300

    first child class

---
+++ psets/pset6/rps.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+
+using namespace std;
+
+class Tool {
+    public:
+        void setStrength(int s) {
+            this->strength = s;
+        };
+
+        void displayStrength() {
+            cout << "Strength: " << this->strength << endl;
+        }
+
+        void displayType() {
+            cout << "Type: " << this->type << endl;
+        }
+
+    protected:
+        int strength;
+        char type;
+
+        virtual void setType() = 0;
+};
+
+class Scissors: public Tool {
+
+    public:
+        Scissors(int s) { 
+            this->setStrength(s);
+            this->setType();
+        }
+
+
+    protected:
+        void setType() {
+            this->type = 's';
+        }
+};
+
+
+/*
+	Implement class Scissors
+*/
+
+/*
+	Implement class Paper
+*/
+
+/*
+	Implement class Rock
+*/
+
+int main() {
+	// Example main function
+	// You may add your own testing code if you like
+    
+    Scissors scissor = Scissors(5);
+    scissor.displayStrength();
+    scissor.displayType();
+
+    /*
+	Scissors s1(5);
+	Paper p1(7);
+	Rock r1(15);
+	cout << s1.fight(p1) << p1.fight(s1) << endl;
+	cout << p1.fight(r1) << r1.fight(p1) << endl;
+	cout << r1.fight(s1) << s1.fight(r1) << endl;
+    */
+
+	return 0;
+}
