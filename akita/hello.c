d115a1907c5b3f1058c7e820081456d0ae4ab54a
vpbmac <vitorpbarbosa7@gmail.com>
2025-02-20 09:21:58 -0300

    pass by reference

--- akita/hello.c
+++ akita/hello.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
-void f2(char hello[]) {
-  printf("from f2: %d\n", &hello);
+void f2(char *hello) {
+  printf("from f2: %d\n", hello);
   printf("%s\n", hello);
 }
 
-void f1(char hello[]) {
-  printf("from f1: %d\n", &hello);
+void f1(char *hello) {
+  printf("from f1: %d\n", hello);
   f2(hello);
 }
 
 int main() {
 
-  char hello[] = "Hello World";
-  printf("from main: %d\n", &hello);
-  f1(&hello);
+  char *hello = "Hello World";
+  printf("from main: %d\n", hello);
+  f1(hello);
 
   return 0;
 }
