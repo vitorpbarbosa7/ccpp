f918ed94d10d1e9d4aec174b783f5628700dc7c3
vpbmac <vitorpbarbosa7@gmail.com>
2025-02-25 07:33:33 -0300

    adding subtree insert before and after method member functions

--- project/bintree.cpp
+++ project/bintree.cpp
@@ -7,9 +7,9 @@
 using namespace std;
 
 class Element {
-  char key;
 
 public:
+  char key;
   Element(char key) { this->key = key; }
   // friend function to overload the << operator
   friend std::ostream &operator<<(std::ostream &os, const Element &e) {
@@ -20,11 +20,11 @@ public:
 class Node {
 
 public:
-  void *item;
+  Element *item;
   Node *parent;
   Node *left;
   Node *right;
-  Node(void *item) {
+  Node(Element *item) {
     this->item = item;
     this->parent = nullptr;
     this->left = nullptr;
@@ -55,6 +55,61 @@ private:
       return this->right;
     }
   }
+
+  void subtree_insert(Node *subtree) {
+
+    // left case, inserts before
+    if (subtree->item->key < this->item->key) {
+      if (this->left) {
+        this->left->subtree_insert(subtree);
+      } else {
+        this->subtree_insert_after(subtree);
+      }
+    }
+
+    // right case, insert after
+    else {
+      if (subtree->item->key > this->item->key) {
+        if (this->right) {
+          this->right->subtree_insert(subtree);
+        } else {
+          this->subtree_insert_after(subtree);
+        }
+      } else {
+        // this->item = subtree->item;
+        return;
+      }
+    }
+  }
+
+  void subtree_insert_before(Node *newnode) {
+
+    if (this->left != nullptr) {
+      // return rightmost node
+      Node *rightMostNode;
+      rightMostNode = this->left->subtree_last();
+      rightMostNode->right = newnode;
+      newnode->parent = rightMostNode;
+    } else {
+      // if it has no left, no difficult on finding the before
+      this->left = newnode;
+      newnode->parent = this;
+    }
+  }
+
+  void subtree_insert_after(Node *newnode) {
+    if (this->right != nullptr) {
+      Node *leftMostNode;
+      leftMostNode = this->right->subtree_first();
+      leftMostNode->left = newnode;
+      newnode->parent = this;
+    } else {
+      this->right = newnode;
+      newnode->parent = this;
+    }
+  }
+
+  // end of Node class
 };
 
 int main() {
